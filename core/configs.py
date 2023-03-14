e45b264db01bd740324f7aff0e1e7e6e50b1f2fb
Aramiz Moura <aramiz.moura.gyn@gmail.com>
2023-03-13 22:03:56 -0300

    alteracao na mensagem

--- core/configs.py
+++ core/configs.py
@@ -6,7 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:postgres@localhost:5433/locacar'
+    DB_URL: str = 'postgresql+asyncpg://postgres:admin@localhost:5432/locacar'
+    DB_PW: str = 'admin'
     DBBaseModel = declarative_base()
 
     class Config:
